93638582774b3d89d7ad128c9bb0896a09fabe69
Ray Toal <rtoal@lmu.edu>
2026-03-16 13:18:19 -0700

    Types!

--- src/interpreter.js
+++ src/interpreter.js
@@ -3,6 +3,44 @@ function error(message, at) {
   throw new Error(`${prefix}${message}`)
 }
 
+function validate(condition, message, at) {
+  if (!condition) {
+    error(message, at)
+  }
+}
+
+function validateBoolean(value, at) {
+  validate(
+    typeof value === "boolean",
+    `Expected a boolean, but got ${typeof value}`,
+    at,
+  )
+}
+
+function validateNumber(value, at) {
+  validate(
+    typeof value === "number",
+    `Expected a number, but got ${typeof value}`,
+    at,
+  )
+}
+
+function validateAlreadyDeclared(memory, name, at) {
+  validate(memory.has(name), `Undefined variable: ${name}`, at)
+}
+
+function validateNotYetDeclared(memory, name, at) {
+  validate(!memory.has(name), `Variable already declared: ${name}`, at)
+}
+
+function validateSameType(target, source, at) {
+  validate(
+    typeof target === typeof source,
+    `Type mismatch: cannot assign ${typeof source} to ${typeof target}`,
+    at,
+  )
+}
+
 export default function interpret(match) {
   const memory = new Map()
 
@@ -13,26 +51,38 @@ export default function interpret(match) {
         statement.eval()
       }
     },
+
     PrintStmt(_print, _open, expression, _close) {
       console.log(expression.eval())
     },
+
     LetStmt(_let, id, _eq, expression) {
+      validateNotYetDeclared(memory, id.sourceString, id.source)
       memory.set(id.sourceString, expression.eval())
     },
+
     IfStmt(_if, expression, _open, statements, _close) {
-      if (expression.eval()) {
+      const test = expression.eval()
+      validateBoolean(test, expression.source)
+      if (test) {
         for (const statement of statements.children) {
           statement.eval()
         }
       }
     },
+
     AssignStmt(id, _eq, expression) {
-      id.eval() // Handles the check for undefined variable in Primary_id
+      const target = id.eval() // Handles the check for undefined variable in Primary_id
+      const source = expression.eval()
+      validateSameType(target, source, id.source)
       memory.set(id.sourceString, expression.eval())
     },
+
     Exp_binary(left, op, right) {
       const x = left.eval()
       const y = right.eval()
+      validateNumber(x, left.source)
+      validateNumber(y, right.source)
       switch (op.sourceString) {
         case "+":
           return x + y
@@ -58,24 +108,41 @@ export default function interpret(match) {
           error(`Unknown operator: ${op.sourceString}`, op.source)
       }
     },
+
     Factor_exponentiation(base, _starStar, exponent) {
-      return base.eval() ** exponent.eval()
+      const x = base.eval()
+      const y = exponent.eval()
+      validateNumber(x, base.source)
+      validateNumber(y, exponent.source)
+      return x ** y
     },
+
     Factor_negation(_minus, factor) {
-      return -factor.eval()
+      const x = factor.eval()
+      validateNumber(x, factor.source)
+      return -x
     },
+
     Primary_parens(_open, expression, _close) {
       return expression.eval()
     },
+
     Primary_id(id) {
       const name = id.sourceString
-      if (!memory.has(name)) {
-        error(`Undefined variable: ${name}`, id.source)
-      }
+      validateAlreadyDeclared(memory, name, id.source)
       return memory.get(name)
     },
+
     num(_digits) {
       return Number(this.sourceString)
+    },
+
+    true(_) {
+      return true
+    },
+
+    false(_) {
+      return false
     },
   }
   const semantics = grammar.createSemantics().addOperation("eval", actions)
--- test/interpreter.test.js
+++ test/interpreter.test.js
@@ -8,7 +8,7 @@ describe("interpreter", () => {
     const sourceCode = `
       print(1) print(2) let x = 5 x = 2
       print((x + 3) ** -2)
-      if x { print(x * 1 / 1 % 1 - 0) } `
+      if true { print(x * 1 / 1 % 1 - 0) } `
     const match = parse(sourceCode)
     let output = []
     console.log = (msg) => {
@@ -19,6 +19,8 @@ describe("interpreter", () => {
   })
   it("should throw an error for undefined variable", () => {
     const sourceCode = `
+      print(true)
+      print(false)
       print(y)
     `
     const match = parse(sourceCode)
@@ -27,5 +29,21 @@ describe("interpreter", () => {
       /Undefined variable: y/,
       "Expected an error for undefined variable, but it did not throw.",
     )
+  })
+  it("should throw an error for variable redefinition", () => {
+    const sourceCode = `
+      let x = 1
+      let x = 2
+    `
+    const match = parse(sourceCode)
+    assert.throws(() => interpret(match), /Variable already declared: x/)
+  })
+  it("should throw an error for type mismatch", () => {
+    const sourceCode = `
+      let x = true
+      print(x + 1)
+    `
+    const match = parse(sourceCode)
+    assert.throws(() => interpret(match), /Expected a number, but got boolean/)
   })
 })
